d6b7c196856289504cfbe6a23a2ed9f83976484d
Héctor Rábago <Hector@Hectors-MacBook-Pro.local>
2016-02-26 18:04:54 -0500

    Week 5

---
+++ Twitter/ProfileCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileCell.swift
+//  Twitter
+//
+//  Created by Héctor Rábago on 2/26/16.
+//  Copyright © 2016 Héctor Rábago. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
